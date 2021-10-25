c5ef3d4129e2a9e23abc1eefe99739adbc492a33
fuqiang.liu <fuqiang.liu@valueonline.cn>
2021-10-25 17:27:16 +0800

    upgrade

---
+++ src/com/liufuqiang/packages/NotificationUtils.java
@@ -0,0 +1,30 @@
+package com.liufuqiang.packages;
+
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
+import com.intellij.openapi.project.Project;
+import org.eclipse.sisu.Nullable;
+
+/** 通知工具类
+ * @author liufuqiang
+ */
+public class NotificationUtils {
+
+    public static void notifyError(@Nullable Project project, String content) {
+        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+                .createNotification(content, NotificationType.ERROR)
+                .notify(project);
+    }
+
+    public static void notifyInfo(@Nullable Project project, String content) {
+        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+                .createNotification(content, NotificationType.INFORMATION)
+                .notify(project);
+    }
+
+    public static void notifyWarn(@Nullable Project project, String content) {
+        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+                .createNotification(content, NotificationType.WARNING)
+                .notify(project);
+    }
+}
